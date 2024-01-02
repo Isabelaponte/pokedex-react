066a789999f154ca2ccf6a6f1330d7d8e52ca311
Isabela de Ponte <isabelapnt01@gmail.com>
2024-01-02 17:59:10 -0300

    build: created project structure

---
+++ src/pages/home/layout/LayoutHome.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import css from './layoutHome.module';
+
+import Header from "../header/Header";
+
+export default function LayoutHome(){
+    return(
+        <div>
+            <Header />
+        </div>
+    )
+}   
